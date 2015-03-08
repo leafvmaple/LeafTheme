7dbe428627c851594bf8d84311cb55d98c3e06e9
leafvmaple <leafvmaple@163.com>
2015-03-09 01:27:45 +0800

    界面更改v1 release

--- archives.php
+++ archives.php
@@ -7,30 +7,26 @@ Template Name: Archives
 <?php get_header(); ?>
 <div id="overcontent">
 
-  <div id="contentarea">
+	<div id="contentarea">
+	
+	<div id="content">
+  
+  <div class="topleftpost"><div class="topright"><div class="bottomleft"><div class="bottomright">
+			<div class="hpost">
+		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+		<h2>Archives by Month:</h2>
+  <ul>
+    <?php wp_get_archives('type=monthly'); ?>
+  </ul>
 
-    <div id="content">
-
-      <div class="topleftpost">
-        <div class="topright">
-          <div class="bottomleft">
-            <div class="bottomright">
-              <div class="hpost">
-                <?php include (TEMPLATEPATH . '/searchform.php'); ?>
-                <h2>Archives by Month:</h2>
-                <ul>
-                  <?php wp_get_archives('type=monthly'); ?></ul>
-
-                <h2>Archives by Subject:</h2>
-                <ul>
-                  <?php wp_list_cats(); ?></ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-    <?php get_sidebar(); ?></div>
+<h2>Archives by Subject:</h2>
+  <ul>
+     <?php wp_list_cats(); ?>
+  </ul>
+</div></div></div></div></div>
+  
+</div>
+<?php get_sidebar(); ?>
+</div> 
 </div>
 <?php get_footer(); ?>
--- footer.php
+++ footer.php
@@ -8,7 +8,8 @@ bloginfo('name');
 bloginfo('rss2_url');
 ?>">RSS News</a> | <a href="<?php
 bloginfo('comments_rss2_url');
-?>">RSS Comments</a> &#149; Theme by <a  href="http://leafvmaple.duapp.com/" title="Leaf WordPress Theme">Leaf Visual</a></div>
+?>">RSS Comments</a> &#149; Theme by <a  href="http://leafvmaple.duapp.com/" title="Leaf WordPress Theme">Leaf Visual</a>
+	</div>
 <div id="designed"><a href="http://leafvmaple.duapp.com/" title="Leaf WordPress Theme"> </a></div>
 </div>
 		<?php
